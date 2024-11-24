b93c2ab79a8923ebaf68c70b3c8aa8de5f1ea775
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2024-11-24 21:33:52 +0700

    Cải thiện nhỏ tốc độ local search

--- src/local_search.cpp
+++ src/local_search.cpp
@@ -8,10 +8,18 @@ bool is_better(const Fitness& lhs, const Fitness& rhs) {
 void first_improvement_permutation_swap_hill_climbing(Individual& individual, const Problem& problem) {
     Fitness current_fitness = fitness(individual, problem);
 
+    int first_drone_customer, count = 0;
+    for (first_drone_customer = 0; count < problem.truck_count() && first_drone_customer < problem.customer_count(); ++first_drone_customer) {
+        if (individual.binary_gene[first_drone_customer] == 1) ++count;
+    }
+
     for (int i = 0; i < individual.permutation_gene.size(); ++i) {
+        bool does_customer_require_truck = problem.does_customer_require_truck(individual.permutation_gene[i]);
         for (int j = i + 1; j < individual.permutation_gene.size(); ++j) {
             Individual clone = individual;
             std::swap(clone.permutation_gene[i], clone.permutation_gene[j]);
+
+            if (problem.does_customer_require_truck(individual.permutation_gene[i]) && j >= first_drone_customer) break;
             if (!is_valid(clone, problem)) continue;
 
             Fitness clone_fitness = fitness(clone, problem);
